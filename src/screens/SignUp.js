dcbd1cf7bd76afbda91b6ed26416d1ed85a44b94
Shivamgulia <92909697+Shivamgulia@users.noreply.github.com>
2024-03-16 23:11:20 +0530

    signup

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -10,9 +10,14 @@ WebBrowser.maybeCompleteAuthSession();
 const SignUp = ({ navigation }) => {
   const [userInfo, setUserInfo] = useState();
 
+  console.log(process.env.EXPO_PUBLIC_GOOGLE_CLIENT_IOS);
+  console.log(process.env.EXPO_PUBLIC_GOOGLE_CLIENT_WEB);
+  console.log(process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ANDROID);
+
   const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ANDROID,
     iosClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_IOS,
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_WEB,
+    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ANDROID,
   });
 
   async function handleSignUpWithGoogle() {
@@ -66,3 +71,76 @@ const styles = StyleSheet.create({
     paddingTop: '20%',
   },
 });
+
+// import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+// import React, { useState, useEffect } from 'react';
+// import * as WebBrowser from 'expo-web-browser';
+// import * as Google from 'expo-auth-session/providers/google';
+
+// import {
+//   GoogleSignin,
+//   GoogleSigninButton,
+//   statusCodes,
+// } from '@react-native-google-signin/google-signin';
+
+// import SignUpForm from '../components/Forms/SignUpForm';
+
+// WebBrowser.maybeCompleteAuthSession();
+
+// const SignUp = ({ navigation }) => {
+//   const [userInfo, setUserInfo] = useState();
+//   const [error, setError] = useState();
+
+//   function configureGoogleSignIn() {
+//     GoogleSignin.configure({
+//       webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_WEB,
+//       androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ANDROID,
+//       iosClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_IOS,
+//     });
+//   }
+
+//   useEffect(() => {
+//     configureGoogleSignIn;
+//   }, []);
+
+//   async function signIn() {
+//     console.log('SignIn');
+//     try {
+//       await GoogleSignin.hasPlayServices();
+//       const user = await GoogleSignin.signIn();
+//       setUserInfo(user);
+//       setError(null);
+//     } catch (e) {
+//       setError(e);
+//     }
+//   }
+
+//   return (
+//     <View style={styles.cont}>
+//       <Text style={styles.Title}>SignUp</Text>
+//       <Button
+//         title='Login'
+//         onPress={() => {
+//           navigation.navigate('Auth');
+//         }}
+//       ></Button>
+//       {!userInfo && (
+//         <GoogleSigninButton
+//           size={GoogleSigninButton.Size.Standard}
+//           color={GoogleSigninButton.Color.Light}
+//           onPress={signIn}
+//         />
+//       )}
+//       {userInfo && <SignUpForm user={userInfo} />}
+//     </View>
+//   );
+// };
+
+// export default SignUp;
+
+// const styles = StyleSheet.create({
+//   cont: {
+//     flex: 1,
+//     paddingTop: '20%',
+//   },
+// });
